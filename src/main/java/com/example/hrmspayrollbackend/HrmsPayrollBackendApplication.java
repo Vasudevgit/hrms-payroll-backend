a1d9ef33e0f65010d7c89ecce4ecc120b4b47bf6
Vasudevgit <vasudev81487@gmail.com>
2026-01-23 14:42:26 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/hrmspayrollbackend/HrmsPayrollBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hrmspayrollbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsPayrollBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrmsPayrollBackendApplication.class, args);
+    }
+
+}
